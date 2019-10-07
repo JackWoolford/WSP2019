6c207771687bdc938ab7369779661fda37a02aeb
JackWoolford <woolfordjack@gmail.com>
2019-10-07 11:28:07 +1100

    PHP Exercises 3

---
+++ php/PHP Exercises 3/dateAndTime.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Time and date</title>
+    </head>
+    <body>
+        <p>
+            <?php echo date('l, F d, Y h:i:sa'); ?>
+        </p>
+    </body>
+</html>
+
